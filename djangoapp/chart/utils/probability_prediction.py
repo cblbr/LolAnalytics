70ea6728ba00c24223805429e9cfc055dbcbc198
lt <lukaszt12@gmail.com>
2020-08-23 15:15:35 +0200

    changed form in getinput

--- djangoapp/chart/utils/probability_prediction.py
+++ djangoapp/chart/utils/probability_prediction.py
@@ -1,24 +1,23 @@
+import pandas as pd
+from xgboost import XGBClassifier
+import os
+
 def predict_probability_of_winning(gold_diff_at_10, exp_diff_at_10, team):
-    from joblib import load
-    import numpy as np
-    import pandas as pd
-    from xgboost import XGBClassifier
 
-    model_blue = XGBClassifier()
-    model_red = XGBClassifier()
-    model_red.load_model('C:/Users/asd/PycharmProjects/ShouldWeSurrender15/djangoapp/chart/utils/models/red_model.json')
-    model_blue.load_model('C:/Users/asd/PycharmProjects/ShouldWeSurrender15/djangoapp/chart/utils/models/blue_model.json')
+    dirname = os.path.dirname(__file__)
+    os.path.join(dirname, 'djangoapp/chart/utils/models/red_model.json')
+    model = XGBClassifier()
 
-    models = {'red': model_red, 'blue': model_blue}
-    dictionary = {team + 'GoldDiff': [gold_diff_at_10], team + 'ExperienceDiff': [
-        exp_diff_at_10]}  # values are casted into lists because pandas constructor doesnt allow scalars
+    model.load_model(os.path.join(dirname, f'./models/{team}_model.json'))
 
-    df = pd.DataFrame(dictionary)
+    df = pd.DataFrame({
+        team + 'GoldDiff': [gold_diff_at_10],
+        team + 'ExperienceDiff': [exp_diff_at_10]
+    })
+    # values are casted into lists because pandas constructor doesnt allow scalars
+    predicts = model.predict_proba(df)
 
-    predicts = models[team].predict_proba(df)
-
-    cols2 = ['redWin', 'blueWin']
-    for i, col in enumerate(cols2):
+    for i, col in enumerate(['redWin', 'blueWin']):
         df[col] = predicts[:, i]
     return df
 # todo: create a model that extrapolates better, maybe ensemble of xgboost and lin reg
--- djangoapp/chart/views.py
+++ djangoapp/chart/views.py
@@ -2,12 +2,6 @@ from django.shortcuts import render
 from django import forms
 from .utils import probability_prediction as pp
 
-choices = (
-    ("1", "red"),
-    ("2", "blue"),
-)
-
-
 # def map_team(team_key):
 #     if team_key == 1:
 #         return "red"
@@ -18,7 +12,12 @@ choices = (
 class NewTaskform(forms.Form):  # form for getting data needed for prediction
     gold_diff = forms.IntegerField(label='Gold difference')
     exp_diff = forms.IntegerField(label='Exp difference')
-    team = forms.CharField(label='team')
+    team = forms.ChoiceField(label='team', choices=
+                             (
+                                 ("red", "red"),
+                                 ("blue", "blue"),
+                             )
+                             )
     #todo: change team variable to be a binary choice
 
 
@@ -28,9 +27,9 @@ def index(request):
         if form.is_valid():
             df = pp.predict_probability_of_winning(form.cleaned_data["gold_diff"],
                                                    form.cleaned_data["exp_diff"], form.cleaned_data["team"])
-            something = round(float(df[f'{form.cleaned_data["team"]}Win']) * 100, ndigits=2)
+            probability = round(float(df[f'{form.cleaned_data["team"]}Win']) * 100, ndigits=2)
             return render(request, 'chart/chart.html',
-                          dict(probability=something))  # "something" is later inputed as gauge value
+                          dict(probability=probability))
     else:
         return render(request, 'chart/getinput.html', {'form': NewTaskform()})
 
