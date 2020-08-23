e6b8c65b2b7cd0944869b3e93c5c0e25cdb56be3
lt <lukaszt12@gmail.com>
2020-08-23 15:51:52 +0200

    deleted pycharm cache files

--- djangoapp/chart/views.py
+++ djangoapp/chart/views.py
@@ -2,23 +2,16 @@ from django.shortcuts import render
 from django import forms
 from .utils import probability_prediction as pp
 
-# def map_team(team_key):
-#     if team_key == 1:
-#         return "red"
-#     else:
-#         return "blue"
-
 
 class NewTaskform(forms.Form):  # form for getting data needed for prediction
     gold_diff = forms.IntegerField(label='Gold difference')
     exp_diff = forms.IntegerField(label='Exp difference')
     team = forms.ChoiceField(label='team', choices=
-                             (
-                                 ("red", "red"),
-                                 ("blue", "blue"),
+    (
+        ("red", "red"),
+        ("blue", "blue"),
+    )
                              )
-                             )
-    #todo: change team variable to be a binary choice
 
 
 def index(request):
